9201c2a6f7acb67aeedd14796aef14756a41d34d
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-02-08 09:52:18 -0600

    Better youtube search

--- src/youtube.ts
+++ src/youtube.ts
@@ -8,13 +8,19 @@ export async function getInfo(url: string) {
 
   const info = await (await ytdl.getInfo(url)).videoDetails;
   let songData = null;
+  console.log(info);
   if (info?.media?.song) {
     const queryTitle = info.media.song;
     const queryArtist = info.media.artist || "";
     songData = await spotify.getSongData(queryTitle + " " + queryArtist, false);
   } else {
+    const channelName = info.author.name.substring(
+      0,
+      info.author.name.indexOf(" - ")
+    );
+    console.log(channelName);
     songData = await spotify.getSongData(
-      info.title + " " + info.ownerChannelName,
+      info.title + " " + channelName || "",
       false
     );
   }
